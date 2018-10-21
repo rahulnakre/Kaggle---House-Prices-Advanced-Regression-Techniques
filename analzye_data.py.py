3a8c2403bb5188150bc415bda60eef5c56601aa7
nakrerah <rahul@DESKTOP-JE2K2EP.localdomain>
2018-10-21 14:17:28 -0400

    First commit. Visualizing data.

---
+++ analzye_data.py.py
@@ -0,0 +1,45 @@
+# used for manipulating directory paths
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression, RidgeCV, LassoCV, ElasticNetCV
+from scipy.stats import skew
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+train = pd.read_csv("train.csv")
+
+# IMPORTANT PRACTISE: duplicate check
+ids_unique = len(set(train.Id))
+ids_total = train.shape[0]
+ids_duplicate  = ids_total - ids_unique
+print("there are " + str(ids_duplicate) + " dulplicate Ids out of " 
+      + str(ids_total))
+# ***** why drop Ids column?
+train.drop("Id", axis = 1, inplace=True)
+
+# check out corr matrix to understand what goin on 
+corr_mat = train.corr()
+#sns.heatmap(corr_mat)
+
+
+# try to notice outliers w regards to GrLivArea (Above Ground Living Area)
+#plt.scatter(train["GrLivArea"], train["SalePrice"], c="blue", marker="s",
+#           alpha=0.1)
+plt.title("Outlier Search")
+plt.xlabel("Above Ground Living Area Sq. Ft (GrLivArea)")
+plt.ylabel("SalePrice")
+
+# try to notice outliers w regards to GrLivArea (Above Ground Living Area)
+plt.scatter(train["LotArea"], train["SalePrice"], c="blue",
+            alpha=0.1)
+plt.title("Outlier Search")
+plt.xlabel("Lot Areaa (Sq.ft)")
+plt.ylabel("SalePrice")
+
+# noticed that stuff above 4000 seems to be an outlier
+# train = train[train["GrLivArea"] < 4000]
+
+
